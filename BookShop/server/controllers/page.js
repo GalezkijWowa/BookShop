a4b3da2d141bbbf8d68edcbfeb68753d684da4c4
Galetsky, Vladimir <v.galetsky@itransition.com>
2019-02-22 15:00:37 +0300

    Implemented CRUD operations for page entiry

--- BookShop/server/controllers/page.js
+++ BookShop/server/controllers/page.js
@@ -1,1 +1,88 @@
-﻿
+﻿const Page = require('../models').Page;
+
+module.exports = {
+    list(req, res) {
+        return Page
+            .findAll({
+                include: [{
+                    model: Page,
+                    as: 'Pages'
+                }],
+            })
+            .then((pages) => res.status(200).send(pages))
+            .catch((error) => { res.status(400).send(error); });
+    },
+
+    getById(req, res) {
+        return Page
+            .findById(req.params.id, {
+                include: [{
+                    model: Page,
+                    as: 'Pages'
+                }],
+            })
+            .then((page) => {
+                if (!page) {
+                    return res.status(404).send({
+                        message: 'Page Not Found',
+                    });
+                }
+                return res.status(200).send(page);
+            })
+            .catch((error) => res.status(400).send(error));
+    },
+
+    add(req, res) {
+        return Page
+            .create({
+                content: req.body.content,
+                number: req.body.number,
+                book_id: req.body.book_id,
+            })
+            .then((page) => res.status(201).send(page))
+            .catch((error) => res.status(400).send(error));
+    },
+
+    update(req, res) {
+        return Page
+            .findById(req.params.id, {
+                include: [{
+                    model: Page,
+                    as: 'Pages'
+                }],
+            })
+            .then(page => {
+                if (!page) {
+                    return res.status(404).send({
+                        message: 'Page Not Found',
+                    });
+                }
+                return page
+                    .update({
+                        content: req.body.content,
+                        number: req.body.number,
+                        book_id: req.body.book_id
+                    })
+                    .then(() => res.status(200).send(page))
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch((error) => res.status(400).send(error));
+    },
+
+    delete(req, res) {
+        return Page
+            .findById(req.params.id)
+            .then(page => {
+                if (!page) {
+                    return res.status(400).send({
+                        message: 'Page Not Found',
+                    });
+                }
+                return page
+                    .destroy()
+                    .then(() => res.status(204).send())
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch((error) => res.status(400).send(error));
+    },
+};
