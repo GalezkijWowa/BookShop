9ebe7f8743883655abb657fa4832542ac2a1113e
Galetsky, Vladimir <v.galetsky@itransition.com>
2019-02-22 15:45:18 +0300

    Implemented book, author CRUD operations plus main controller

--- BookShop/server/controllers/author.js
+++ BookShop/server/controllers/author.js
@@ -1,82 +1,82 @@
-﻿const Book = require('../models').Book;
+﻿const Author = require('../models').Author;
 
 module.exports = {
     list(req, res) {
-        return Book
+        return Author
             .findAll({
                 include: [{
-                    model: Book,
-                    as: 'Books'
+                    model: Author,
+                    as: 'Authors'
                 }],
             })
-            .then((books) => res.status(200).send(books))
+            .then((authors) => res.status(200).send(authors))
             .catch((error) => { res.status(400).send(error); });
     },
 
     getById(req, res) {
-        return Book
+        return Author
             .findById(req.params.id, {
                 include: [{
-                    model: Book,
-                    as: 'Books'
+                    model: Author,
+                    as: 'Authors'
                 }],
             })
-            .then((book) => {
-                if (!book) {
+            .then((author) => {
+                if (!author) {
                     return res.status(404).send({
-                        message: 'Book Not Found',
+                        message: 'Author Not Found',
                     });
                 }
-                return res.status(200).send(book);
+                return res.status(200).send(author);
             })
             .catch((error) => res.status(400).send(error));
     },
 
     add(req, res) {
-        return Book
+        return Author
             .create({
-                title: req.body.title,
-                cost: req.body.cost
+                name: req.body.name,
+                age: req.body.age
             })
-            .then((book) => res.status(201).send(book))
+            .then((author) => res.status(201).send(author))
             .catch((error) => res.status(400).send(error));
     },
 
     update(req, res) {
-        return Book
+        return Author
             .findById(req.params.id, {
                 include: [{
-                    model: Book,
-                    as: 'Books'
+                    model: Author,
+                    as: 'Authors'
                 }],
             })
-            .then(book => {
-                if (!book) {
+            .then(author => {
+                if (!author) {
                     return res.status(404).send({
-                        message: 'Book Not Found',
+                        message: 'Author Not Found',
                     });
                 }
-                return book
+                return author
                     .update({
-                        title: req.body.title,
-                        cost: req.body.cost
+                        name: req.body.name,
+                        age: req.body.age
                     })
-                    .then(() => res.status(200).send(book))
+                    .then(() => res.status(200).send(author))
                     .catch((error) => res.status(400).send(error));
             })
             .catch((error) => res.status(400).send(error));
     },
 
     delete(req, res) {
-        return Book
+        return Author
             .findById(req.params.id)
-            .then(book => {
-                if (!book) {
+            .then(author => {
+                if (!author) {
                     return res.status(400).send({
-                        message: 'Book Not Found',
+                        message: 'Author Not Found',
                     });
                 }
-                return book
+                return author
                     .destroy()
                     .then(() => res.status(204).send())
                     .catch((error) => res.status(400).send(error));
--- BookShop/server/controllers/index.js
+++ BookShop/server/controllers/index.js
@@ -1,1 +1,9 @@
-﻿
+﻿const book = require('./book');
+const author = require('./author');
+const page = require('./page');
+
+module.exports = {
+    book,
+    author,
+    page,
+};
