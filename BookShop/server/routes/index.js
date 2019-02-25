ccaa06722367940349ad8b358378189ff1f2929f
Galetsky, Vladimir <v.galetsky@itransition.com>
2019-02-25 18:33:44 +0300

    Added swagger-ui support

--- BookShop/server/routes/index.js
+++ BookShop/server/routes/index.js
@@ -5,8 +5,10 @@ const authorRouter = require('./author');
 const bookRouter = require('./book');
 const pageRouter = require('./page');
 const authRouter = require('./auth');
+const swaggerRouter = require('./swagger');
 
 router.use(authRouter);
+router.use(swaggerRouter);
 router.use(authorRouter);
 router.use(pageRouter);
 router.use(bookRouter);
---
+++ BookShop/server/routes/swagger.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+router.use('/swagger', swaggerUi.serve);
+router.get('/swagger', swaggerUi.setup(swaggerDocument));
+
+module.exports = router;
