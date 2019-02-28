a080faca42b4a7417a5dd8b1afafaf14e1810b5d
Galetsky, Vladimir <v.galetsky@itransition.com>
2019-02-28 19:25:11 +0300

    Fixed sing in test

--- BookShop/test/auth.js
+++ BookShop/test/auth.js
@@ -3,23 +3,31 @@ const chaiHttp = require('chai-http');
 const app = require('../app');
 const should = chai.should();
 chai.use(chaiHttp)
-
+const authService = require("../server/services/authService");
 
 describe('AUTH/SINGIN', () => {
-    it("should singin user", (done) => {
-        const user = {
-            username: "test",
-            password: "123456",
-        }
-        chai.request(app)
-            .post('/api/auth/singin')
-            .send(user)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                done();
+    const userCreds = {
+        username: Math.random().toString(36).substr(2, 6),
+        password: "123456",
+    }
+
+    authService.create(userCreds.username, userCreds.password)
+        .then(function (user) {
+            const userCredsForLogin = {
+                username: userCreds.username,
+                password: userCreds.password,
+            }
+            it("should singin user", (done) => {
+                chai.request(app)
+                    .post('/api/auth/singin')
+                    .send(user)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        done();
+                    });
             });
-    });
+        });
 });
 
 describe('AUTH/REGISTER', () => {
