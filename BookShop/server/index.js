d5462e9a297fa02c2f0195420f383d2169a9812b
Vladimir <galezkij@yandex.ru>
2019-03-01 15:09:00 +0300

    PR fixes

--- BookShop/server/index.js
+++ BookShop/server/index.js
@@ -1,30 +1,29 @@
 ﻿let logger = require("morgan"),
     cookieParser = require("cookie-parser"),
     bodyParser = require("body-parser"),
-    routes = require("../routes");
-
+    routes = require("./routes");
+    
 module.exports = function (app, express) {
-    app.use(logger("dev"));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-
+    app.use(logger('dev'));
     app.use(routes);
 
-    app.use(function (req, res, next) {
+    app.use((req, res, next) => {
         let err = new Error("Not Found");
         err.status = 404;
         next(err);
     });
 
     if (app.get("env") === "development") {
-        app.use(function (err, req, res, next) {
+        app.use((err, req, res, next) => {
             res.status(err.status || 500);
             res.status(404).send();
         });
     }
 
-    app.use(function (err, req, res, next) {
+    app.use((err, req, res, next) => {
         res.status(err.status || 500);
         res.status(404).send();
     });
--- BookShop/server/services/authService.js
+++ BookShop/server/services/authService.js
@@ -1,4 +1,6 @@
 ﻿const User = require("../database/models").User;
+const config = require("../config");
+const jwt = require("jsonwebtoken");
 
 function findByName(username) {
     return User.find({
@@ -11,6 +13,22 @@ function create(username, password) {
         .create({ username, password })
 }
 
+function getToken(user) {
+    return jwt.sign(JSON.parse(JSON.stringify(user)), config.get("jwtSecretKey"), { expiresIn: config.get("jwtExpires") });  // jwtSecretKey must migrate to env variables(problem with test)
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, config.get("jwtSecretKey"), (err, data) => { // jwtSecretKey must migrate to env variables(problem with test)
+        if (err) {
+            return err;
+        }
+        else {
+            return data;
+        }
+    });
+}
 
 module.exports.findByName = findByName;
 module.exports.create = create;
+module.exports.verifyToken = verifyToken;
+module.exports.getToken = getToken;
